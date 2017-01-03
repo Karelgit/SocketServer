92a2ab533753b8c56ab6f859c72f5f4bc011b1c2
huanghai <huanghai@simpletech.com.cn>
2017-01-03 20:02:16 +0800

    客户端测试成功

--- src/main/java/NewsPusherModule/server/Server.java
+++ src/main/java/NewsPusherModule/server/Server.java
@@ -224,52 +224,7 @@ public class Server {
 
     }
 
-    /**
-     * MRP推送信息转换PushInfo
-     * @param articlesJSON
-     * @Param userJSON
-     *
-     */
-    private PushInfo convertToPushInfo(final String articlesJSON,final String userJSON){
-        if(articlesJSON==null||"".equals(articlesJSON)||userJSON==null||"".equals(userJSON)){
-            throw new IllegalArgumentException("articles and duocaiInfo's json content can not be null or empty ");
-        }
-        final JSONObject duocaiMap = JSON.parseObject(userJSON);
-        final PushInfo pushInfo = new PushInfo();
-        final String initEditor = duocaiMap.getString("initEditor");
-        final String password = duocaiMap.getString("password");
-        final String userName = duocaiMap.getString("userName");
-        if (initEditor == null ||"".equals(initEditor)||password==null||"".equals(password)||userName==null||"".equals(userName)){
-            throw  new IllegalArgumentException("initEditor,password,userName in json string can not be null or empty");
-        }
-        pushInfo.setUserName(userName);
-        pushInfo.setInitEditor(initEditor);
-        pushInfo.setPassword(password);
-        pushInfo.setArticlesJSON(articlesJSON);
-        pushInfo.setCustomerInfoJSON(userJSON);
-        return  pushInfo;
-    }
 
-    /**
-     * 推送新闻列表
-     * @param articlesJSON
-     * @param userJSON
-     */
-    public boolean exportNews(String articlesJSON,String userJSON){
-        final  PushInfo pushInfo = convertToPushInfo(articlesJSON,userJSON);
-        try {
-            Socket socket = new Socket(resourceBundle.getString("SERVER_IP"),Integer.parseInt(resourceBundle.getString("SERVER_PORT")));
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            oos.writeObject(pushInfo);
-            System.out.println("发送：\t"+pushInfo);
-            oos.flush();
-            socket.close();
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
 
 
 }
