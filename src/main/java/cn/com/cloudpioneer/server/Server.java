bb7eb7298dfe2d439acf171ca7c66ac7ceddd8c3
tijunwang <15761635397@163.com>
2017-01-03 15:26:54 +0800

    封装推送信息

--- src/main/java/cn/com/cloudpioneer/server/Server.java
+++ src/main/java/cn/com/cloudpioneer/server/Server.java
@@ -1,8 +1,9 @@
-package com.gengyun.server;
+package cn.com.cloudpioneer.server;
 
+import cn.com.cloudpioneer.entity.HandShaker;
+import cn.com.cloudpioneer.entity.PushInfo;
 import com.alibaba.fastjson.JSON;
-import com.gengyun.model.HandShaker;
-import com.gengyun.model.PushInfo;
+import com.alibaba.fastjson.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -203,5 +204,46 @@ public class Server {
         }
 
     }
+
+    /**
+     *
+     * @param articlesJSON
+     * @Param userJSON
+     *
+     */
+    private PushInfo convertToPushInfo(final String articlesJSON,final String userJSON){
+        if(articlesJSON==null||"".equals(articlesJSON)||userJSON==null||"".equals(userJSON)){
+            throw new IllegalArgumentException("articles and duocaiInfo's json content can not be null or empty ");
+        }
+        final JSONObject duocaiMap = JSON.parseObject(userJSON);
+        final PushInfo pushInfo = new PushInfo();
+        final String initEditor = duocaiMap.getString("initEditor");
+        final String password = duocaiMap.getString("password");
+        final String userName = duocaiMap.getString("userName");
+        if (initEditor == null ||"".equals(initEditor)||password==null||"".equals(password)||userName==null||"".equals(userName)){
+            throw  new IllegalArgumentException("initEditor,password,userName in json string can not be null or empty");
+        }
+        pushInfo.setUserName(userName);
+        pushInfo.setInitEditor(initEditor);
+        pushInfo.setPassword(password);
+        pushInfo.setArticlesJSON(articlesJSON);
+        pushInfo.setCustomerInfoJSON(userJSON);
+        return  pushInfo;
+    }
+
+    public void exportNews(String articlesJSON,String userJSON){
+        final  PushInfo pushInfo = convertToPushInfo(articlesJSON,userJSON);
+        try {
+            Socket socket = new Socket("127.0.0.1",65432);
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(pushInfo);
+            System.out.println("发送：\t"+pushInfo);
+            oos.flush();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
